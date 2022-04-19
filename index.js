ad6d13d2febb0207a2c52dda90690958cad35008
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-04-19 17:52:27 -0500

    add/ expading funtionality done and screenshota a video demo

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Linked')
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach( panel =>{
+  panel.addEventListener('click', ()=>{
+    removeActiveClasses();
+    panel.classList.add('active');
+  })
+})
+
+function removeActiveClasses(){
+  panels.forEach(panel =>{
+    panel.classList.remove('active');
+  })
+}
